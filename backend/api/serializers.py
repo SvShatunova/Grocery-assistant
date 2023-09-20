63d9f0aa50813be99e58e3d82cdface63728eff2
svshatunova <svshatun0va@yandex.ru>
2023-09-20 19:47:18 +0300

    foodgram

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -174,7 +174,7 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
         return recipe
 
     def update(self, instance, validated_data):
-        recipe = instance
+        # recipe = instance
         instance.image = validated_data.get('image', instance.image)
         instance.name = validated_data.get('name', instance.name)
         instance.text = validated_data.get('text', instance.name)
@@ -183,14 +183,20 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
             instance.cooking_time
         )
         instance.tags.clear()
-        instance.ingredients.clear()
-        tags = validated_data.get('tags')
-        instance.tags.set(tags)
-        ingredients = validated_data.get('ingredients')
-        IngredientAmount.objects.filter(recipe=recipe).delete()
-        self.create_ingredients_amount(ingredients, recipe)
+        tags_data = self.initial_data.get('tags')
+        instance.tags.set(tags_data)
+        IngredientAmount.objects.filter(recipe=instance).all().delete()
+        self.create_ingredients(validated_data.get('ingredients'), instance)
         instance.save()
         return instance
+        # instance.ingredients.clear()
+        # tags = validated_data.get('tags')
+        # instance.tags.set(tags)
+        # ingredients = validated_data.get('ingredients')
+        # IngredientAmount.objects.filter(recipe=recipe).delete()
+        # self.create_ingredients_amount(ingredients, recipe)
+        # instance.save()
+        # return instance
 
     def to_representation(self, instance):
         return RecipeReadSerializer(instance, context={
